80798b7130fa8b7fb7f4db7f6866a6c8464b42dd
KaranGoel59 <karangoel59@gmail.com>
2024-08-23 13:03:07 +1000

    format

--- A1/submit/main.py
+++ A1/submit/main.py
@@ -7,14 +7,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 
-from sklearn.model_selection import train_test_split, KFold, cross_val_score, StratifiedKFold, GridSearchCV
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold, GridSearchCV
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score, classification_report
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
 from sklearn.cluster import KMeans, AgglomerativeClustering
 from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score
 from sklearn.metrics import pairwise_distances_argmin_min
 from sklearn.tree import DecisionTreeClassifier
+
 
 class Setup:
     """Class to Setup"""
@@ -85,10 +86,12 @@ class Task1:
     def scale_features(self, df):
         """Scale numeric features using StandardScaler"""
         print("Applying Standard Scaler")
-        columns_to_scale = ['current_price', 'raw_price', 'discount', 'likes_count']
+        columns_to_scale = ['current_price',
+                            'raw_price', 'discount', 'likes_count']
         numerical = df[columns_to_scale]
         scaled_features = self.scaler.fit_transform(numerical)
-        scaled_features_df = pd.DataFrame(scaled_features, columns=columns_to_scale, index=self.df.index)
+        scaled_features_df = pd.DataFrame(
+            scaled_features, columns=columns_to_scale, index=self.df.index)
         df[columns_to_scale] = scaled_features_df
 
         return df
@@ -101,7 +104,7 @@ class Task1:
 
         return df
 
-    def remove_outliers(self,df):
+    def remove_outliers(self, df):
         """Remove outliers based on Z-score"""
         print("Removed outliers")
         df_numerical = df.select_dtypes(include=[np.number])
@@ -112,7 +115,7 @@ class Task1:
 
         return df
 
-    def plot_pairplot(self , df):
+    def plot_pairplot(self, df):
         """Plot the scatterplot matrix"""
         sns.pairplot(df)
         plt.suptitle('Scatterplot Matrix', y=1.02)  # Add a title
@@ -124,13 +127,15 @@ class Task1:
         correlation_matrix = df.corr()
 
         # Initialize a DataFrame to store p-values
-        p_values = pd.DataFrame(np.zeros_like(correlation_matrix), columns=correlation_matrix.columns, index=correlation_matrix.index)
+        p_values = pd.DataFrame(np.zeros_like(
+            correlation_matrix), columns=correlation_matrix.columns, index=correlation_matrix.index)
 
         # Calculate p-values
         for row in correlation_matrix.index:
             for col in correlation_matrix.index:
                 if row == col:
-                    p_values.loc[row, col] = 0.0  # p-value is 0 for diagonal elements
+                    # p-value is 0 for diagonal elements
+                    p_values.loc[row, col] = 0.0
                 else:
                     _, p_val = stats.pearsonr(df[row], df[col])
 
@@ -160,7 +165,7 @@ class Task1:
         else:
             print("Fail to reject the null hypothesis: There is no significant correlation between current_price and likes_count.")
 
-    def hypothesis2_discount_likes(self , p_values):
+    def hypothesis2_discount_likes(self, p_values):
         p_value = p_values['discount']['likes_count']
         print("P-value:", p_value)
 
@@ -170,8 +175,9 @@ class Task1:
         else:
             print("Fail to reject the null hypothesis: There is no significant correlation between discount and likes_count.")
 
-    def hypothesis3_likes_category(self , df):
-        likes_count_by_category = [group['likes_count'].values for name, group in self.df.groupby('category')]
+    def hypothesis3_likes_category(self, df):
+        likes_count_by_category = [
+            group['likes_count'].values for name, group in self.df.groupby('category')]
         f_statistic, p_value = stats.f_oneway(*likes_count_by_category)
 
         print("ANOVA F-statistic:", f_statistic)
@@ -183,7 +189,8 @@ class Task1:
         else:
             print("Fail to reject the null hypothesis: The average likes_count is the same across all product categories.")
 
-        df['category'] = self.le_category.inverse_transform(df['category'].to_numpy())
+        df['category'] = self.le_category.inverse_transform(
+            df['category'].to_numpy())
 
         plt.figure(figsize=(12, 8))
         sns.boxplot(x='category', y='likes_count', data=self.df)
@@ -197,7 +204,8 @@ class Task1:
         """Analyze top categories and products based on likes_count"""
 
         def get_top_categories_by_likes(df, top_n):
-            avg_likes_per_category = df.groupby('category')['likes_count'].mean().sort_values(ascending=False)
+            avg_likes_per_category = df.groupby(
+                'category')['likes_count'].mean().sort_values(ascending=False)
             top_categories = avg_likes_per_category.head(top_n).index.tolist()
             return top_categories
 
@@ -214,15 +222,20 @@ class Task1:
         filtered_df = filter_top_categories(df, top_categories)
         top_products = get_top_products(filtered_df, top_n=10)
 
-        print("Top 7 Categories (based on average likes count):", self.le_category.inverse_transform(top_categories))
+        print("Top 7 Categories (based on average likes count):",
+              self.le_category.inverse_transform(top_categories))
         print()
-        print("Top 10 Products in the Top 7 Categories:\n", self.le_name.inverse_transform(top_products['name'].to_list()))
+        print("Top 10 Products in the Top 7 Categories:\n",
+              self.le_name.inverse_transform(top_products['name'].to_list()))
 
-        top_products['category'] = self.le_category.inverse_transform(top_products['category'].to_numpy())
+        top_products['category'] = self.le_category.inverse_transform(
+            top_products['category'].to_numpy())
         return top_products
+
 
 class Task2:
     """Clustering"""
+
     def __init__(self, df, top_products):
         # Drop non-required columns
         self.df_cluster = df.drop(['raw_price', 'likes_count'], axis=1)
@@ -236,10 +249,9 @@ class Task2:
             "Calinski-Harabasz Index": {},
             "Davies-Bouldin Index": {}
         }
-        self.kmeans_model = None 
+        self.kmeans_model = None
         self.agglomerative_labels = None
-        self.centroids = None 
-
+        self.centroids = None
 
     def elbow_method(self):
         wss = []
@@ -264,15 +276,18 @@ class Task2:
             labels = algorithm.fit_predict(self.X)
 
             if name == "KMeans":
-                self.kmeans_model = algorithm  
+                self.kmeans_model = algorithm
             elif name == "Agglomerative":
                 self.agglomerative_labels = labels
                 self.compute_centroids(labels)
 
             # Store the calculated metrics
-            self.metrics["Silhouette Score"][name] = silhouette_score(self.X, labels)
-            self.metrics["Calinski-Harabasz Index"][name] = calinski_harabasz_score(self.X, labels)
-            self.metrics["Davies-Bouldin Index"][name] = davies_bouldin_score(self.X, labels)
+            self.metrics["Silhouette Score"][name] = silhouette_score(
+                self.X, labels)
+            self.metrics["Calinski-Harabasz Index"][name] = calinski_harabasz_score(
+                self.X, labels)
+            self.metrics["Davies-Bouldin Index"][name] = davies_bouldin_score(
+                self.X, labels)
 
             # Create a copy of the data with the new labels
             df_copy = self.X.copy()
@@ -300,38 +315,44 @@ class Task2:
     def predict_cluster_kmeans(self, current_price, discount):
         """Predict the cluster using KMeans"""
         if self.kmeans_model is None:
-            raise ValueError("KMeans model has not been trained. Call perform_clustering() first.")
-        input_data = pd.DataFrame({'current_price': [current_price], 'discount': [discount]})
+            raise ValueError(
+                "KMeans model has not been trained. Call perform_clustering() first.")
+        input_data = pd.DataFrame(
+            {'current_price': [current_price], 'discount': [discount]})
         predicted_cluster = self.kmeans_model.predict(input_data)
         return predicted_cluster[0]
 
     def predict_cluster_agglomerative(self, current_price, discount):
         """Predict the cluster using Agglomerative Clustering"""
         if self.centroids is None:
-            raise ValueError("Centroids have not been computed. Call perform_clustering() first.")
+            raise ValueError(
+                "Centroids have not been computed. Call perform_clustering() first.")
         input_data = np.array([[current_price, discount]])
         closest, _ = pairwise_distances_argmin_min(input_data, self.centroids)
         return closest[0]
 
     def map_top_products_to_clusters(self, le_name, le_category):
         if self.kmeans_model is None and self.agglomerative_labels is None:
-            raise ValueError("Clustering has not been performed. Call perform_clustering() first.")
-        
+            raise ValueError(
+                "Clustering has not been performed. Call perform_clustering() first.")
+
         top_products_df = self.X.copy()
         top_products_df['name'] = self.pnames
         top_products_df['category'] = self.categories
 
         top_products_df['kmeans_cluster'] = self.kmeans_model.predict(self.X)
-        
+
         top_products_df['agglomerative_cluster'] = self.agglomerative_labels
 
-        top_products_clusters = top_products_df[top_products_df['name'].isin(self.top_products['name'].to_list())]
+        top_products_clusters = top_products_df[top_products_df['name'].isin(
+            self.top_products['name'].to_list())]
 
-        top_products_clusters['name'] = le_name.inverse_transform(top_products_clusters['name'].to_numpy())
-        top_products_clusters['category'] = le_category.inverse_transform(top_products_clusters['category'].to_numpy())
+        top_products_clusters['name'] = le_name.inverse_transform(
+            top_products_clusters['name'].to_numpy())
+        top_products_clusters['category'] = le_category.inverse_transform(
+            top_products_clusters['category'].to_numpy())
 
         return top_products_clusters
-
 
     def visualize_clusters(self, df_copy, df_mean, labels, algorithm_name):
         fig, ax = plt.subplots(figsize=(20, 8))
@@ -349,7 +370,8 @@ class Task2:
         )
 
         # Mark top products with a large red dot
-        top_products_df = df_copy[df_copy['name'].isin(self.top_products['name'].to_list())]
+        top_products_df = df_copy[df_copy['name'].isin(
+            self.top_products['name'].to_list())]
         ax.scatter(
             x=top_products_df[columns[0]],
             y=top_products_df[columns[1]],
@@ -375,11 +397,14 @@ class Task2:
                 print(f"{algo_name}: {value:.4f}")
 
         # Determine the best algorithm based on the Silhouette Score
-        best_algorithm = max(self.metrics["Silhouette Score"], key=self.metrics["Silhouette Score"].get)
+        best_algorithm = max(
+            self.metrics["Silhouette Score"], key=self.metrics["Silhouette Score"].get)
         print(f"\nBest Algorithm based on Silhouette Score: {best_algorithm}")
+
 
 class Task3:
     """Classification"""
+
     def __init__(self, df):
         self.df = df.copy()
         self.X_train = None
@@ -389,20 +414,23 @@ class Task3:
         self.knn_model = None
         self.dt_model = None
         self.skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
-    
+
     def preprocess_data(self, le_category):
         print("Creating popularity column based on likes_count")
-        self.df['popularity'] = self.df['likes_count'].apply(lambda x: 1 if x > self.df['likes_count'].mean() else 0)
-        
+        self.df['popularity'] = self.df['likes_count'].apply(
+            lambda x: 1 if x > self.df['likes_count'].mean() else 0)
+
         # Selecting features and target
-        print("Selecting features and target ['category', 'current_price', 'discount']")
+        print(
+            "Selecting features and target ['category', 'current_price', 'discount']")
         X = self.df[['category', 'current_price', 'discount']]
         X['category'] = le_category.fit_transform(X['category'])
         y = self.df['popularity']
-        
+
         # Train-test split
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42)
+
     def train_knn(self):
         print("KNN")
         knn = KNeighborsClassifier()
@@ -411,18 +439,20 @@ class Task3:
             'weights': ['uniform'],
             'metric': ['manhattan']
         }
-        grid_search = GridSearchCV(knn, param_grid, cv=self.skf, scoring='accuracy')
+        grid_search = GridSearchCV(
+            knn, param_grid, cv=self.skf, scoring='accuracy')
         grid_search.fit(self.X_train, self.y_train)
-        
+
         # Best KNN model
         self.knn_model = grid_search.best_estimator_
         print(f"Best KNN Parameters: {grid_search.best_params_}")
-        
+
         # Cross-validation scores
-        cv_scores = cross_val_score(self.knn_model, self.X_train, self.y_train, cv=self.skf, scoring='accuracy')
+        cv_scores = cross_val_score(
+            self.knn_model, self.X_train, self.y_train, cv=self.skf, scoring='accuracy')
         print(f'KNN Average Accuracy: {cv_scores.mean() * 100:.2f}%')
         print(f'KNN Standard Deviation: {cv_scores.std() * 100:.2f}%')
-    
+
     def evaluate_knn(self):
         self.knn_model.fit(self.X_train, self.y_train)
         y_pred = self.knn_model.predict(self.X_test)
@@ -431,14 +461,14 @@ class Task3:
         recall = recall_score(self.y_test, y_pred)
         f1 = f1_score(self.y_test, y_pred)
         cm = confusion_matrix(self.y_test, y_pred)
-        
+
         print(f'KNN Accuracy: {accuracy * 100:.2f}%')
         print(f'KNN Precision: {precision:.2f}')
         print(f'KNN Recall: {recall:.2f}')
         print(f'KNN F1-Score: {f1:.2f}')
         print('KNN Confusion Matrix:')
         print(cm)
-        
+
         # Plot confusion matrix
         plt.figure(figsize=(8, 6))
         sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
@@ -446,15 +476,16 @@ class Task3:
         plt.ylabel('Actual')
         plt.title('Confusion Matrix for KNN Popularity Prediction')
         plt.show()
-        
+
         # Plot precision, recall, and f1-score
-        metrics = pd.DataFrame({'Metric': ['Precision', 'Recall', 'F1-Score'], 'Score': [precision, recall, f1]})
+        metrics = pd.DataFrame(
+            {'Metric': ['Precision', 'Recall', 'F1-Score'], 'Score': [precision, recall, f1]})
         plt.figure(figsize=(8, 6))
         sns.barplot(x='Metric', y='Score', data=metrics, palette='viridis')
         plt.ylim(0, 1)
         plt.title('KNN Precision, Recall, and F1-Score')
         plt.show()
-    
+
     def train_decision_tree(self):
         print("Decision Tree")
         dt = DecisionTreeClassifier(random_state=42)
@@ -464,18 +495,21 @@ class Task3:
             'min_samples_split': [2, 5, 10, 15],
             'min_samples_leaf': [1, 2, 4, 6]
         }
-        grid_search = GridSearchCV(dt, param_grid, cv=self.skf, n_jobs=-1, verbose=2)
+        grid_search = GridSearchCV(
+            dt, param_grid, cv=self.skf, n_jobs=-1, verbose=2)
         grid_search.fit(self.X_train, self.y_train)
-        
+
         # Best Decision Tree model
         self.dt_model = grid_search.best_estimator_
         print(f"Best Decision Tree Parameters: {grid_search.best_params_}")
-        
+
         # Cross-validation scores
-        cv_scores = cross_val_score(self.dt_model, self.X_train, self.y_train, cv=self.skf, scoring='accuracy')
+        cv_scores = cross_val_score(
+            self.dt_model, self.X_train, self.y_train, cv=self.skf, scoring='accuracy')
         print(f'Decision Tree Average Accuracy: {cv_scores.mean() * 100:.2f}%')
-        print(f'Decision Tree Standard Deviation: {cv_scores.std() * 100:.2f}%')
-    
+        print(f'Decision Tree Standard Deviation: {
+              cv_scores.std() * 100:.2f}%')
+
     def evaluate_decision_tree(self):
         self.dt_model.fit(self.X_train, self.y_train)
         y_pred = self.dt_model.predict(self.X_test)
@@ -484,14 +518,14 @@ class Task3:
         recall = recall_score(self.y_test, y_pred)
         f1 = f1_score(self.y_test, y_pred)
         cm = confusion_matrix(self.y_test, y_pred)
-        
+
         print(f'Decision Tree Accuracy: {accuracy * 100:.2f}%')
         print(f'Decision Tree Precision: {precision:.2f}')
         print(f'Decision Tree Recall: {recall:.2f}')
         print(f'Decision Tree F1-Score: {f1:.2f}')
         print('Decision Tree Confusion Matrix:')
         print(cm)
-        
+
         # Plot confusion matrix
         plt.figure(figsize=(8, 6))
         sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
@@ -499,9 +533,10 @@ class Task3:
         plt.ylabel('Actual')
         plt.title('Confusion Matrix for Decision Tree Popularity Prediction')
         plt.show()
-        
+
         # Plot precision, recall, and f1-score
-        metrics = pd.DataFrame({'Metric': ['Precision', 'Recall', 'F1-Score'], 'Score': [precision, recall, f1]})
+        metrics = pd.DataFrame(
+            {'Metric': ['Precision', 'Recall', 'F1-Score'], 'Score': [precision, recall, f1]})
         plt.figure(figsize=(8, 6))
         sns.barplot(x='Metric', y='Score', data=metrics, palette='viridis')
         plt.ylim(0, 1)
@@ -517,7 +552,7 @@ def main():
 
     base_path = r'.'
     files = ['accessories.csv', 'bags.csv', 'beauty.csv', 'house.csv',
-            'jewelry.csv', 'kids.csv', 'men.csv', 'shoes.csv', 'women.csv']
+             'jewelry.csv', 'kids.csv', 'men.csv', 'shoes.csv', 'women.csv']
     task1 = Task1(base_path, files)
 
     Setup.section("Processing Data")
@@ -535,7 +570,6 @@ def main():
     Setup.section("Hypothesis 3")
     task1.hypothesis3_likes_category(df.copy())
 
-
     task1.plot_pairplot(df)
 
     Setup.section("Top categories and products")
@@ -547,8 +581,10 @@ def main():
     task2.elbow_method()
     task2.perform_clustering()
     task2.print_metrics()
-    top_products_clusters = task2.map_top_products_to_clusters(task1.le_name, task1.le_category)
-    print(top_products_clusters[['name','category','kmeans_cluster','agglomerative_cluster']])
+    top_products_clusters = task2.map_top_products_to_clusters(
+        task1.le_name, task1.le_category)
+    print(top_products_clusters[['name', 'category',
+          'kmeans_cluster', 'agglomerative_cluster']])
 
     Setup.section("Classification")
 
@@ -558,7 +594,6 @@ def main():
     task3.evaluate_knn()
     task3.train_decision_tree()
     task3.evaluate_decision_tree()
-
 
 
 if __name__ == "__main__":
